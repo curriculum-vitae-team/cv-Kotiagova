46043d6b0adb31bc4584f9a7f7e12bb7915430b9
Uladzislau Yarmashuk <vlad.yarmashuk@scentbird.com>
2022-10-24 09:48:09 +0300

    fix app execution

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['craco.config.js', '.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
