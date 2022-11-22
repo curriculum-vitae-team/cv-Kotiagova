8b14d08701270083e5913f19633d67a770f3b35b
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-22 21:04:31 +0300

    Implement CRUD functions

--- src/pages/EmployeesPage/EmployeesPage.tsx
+++ src/pages/EmployeesPage/EmployeesPage.tsx
@@ -1,35 +1,74 @@
-import EmployeesList from '@/components/EmployeesList/EmployeesList'
+import EmployeesList, { Employee } from '@/components/EmployeesList/EmployeesList'
 import React, { useState } from 'react'
 
 import { Button, Typography } from 'antd'
 import Search from 'antd/lib/input/Search'
 import { mockEmployeeList } from './mocks/employeeList'
 import { nanoid } from 'nanoid'
+import NewEmployeeModal from '@/components/NewEmployeeModal/NewEmployeeModal'
+import DeleteEmployeeModal from '@/components/DeleteEmployeeModal/DeleteEmployeeModal'
 
 const { Title } = Typography
 
 const EmployeesPage = () => {
   const [searchedEmployee, setSearchedEmployee] = useState('')
-  const [employeeList, setEmployeeList] = useState(mockEmployeeList)
+  const [isNewEmployeeModalOpen, setIsNewEmployeeModalOpen] = useState(false)
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+  const [employeeList, setEmployeeList] = useState<Employee[]>(mockEmployeeList)
+  const [newEmployeeContent, setNewEmployeeContent] = useState<Employee>({
+    key: nanoid(),
+    firstName: '',
+    lastName: '',
+    email: '',
+    department: '',
+    specialization: ''
+  })
+
+  const handleAddEmployeeButtonClick = () => {
+    setIsNewEmployeeModalOpen(true)
+    setNewEmployeeContent({
+      key: nanoid(),
+      firstName: '',
+      lastName: '',
+      email: '',
+      department: '',
+      specialization: ''
+    })
+  }
 
   const addEmployee = () => {
-    setEmployeeList((prevEmployeeList) =>
-      prevEmployeeList.concat({
-        key: nanoid(),
-        firstName: 'Anton',
-        lastName: 'Antonov',
-        department: 'IOS',
-        email: 'Anton.Antonov@gmail.com',
-        specialization: 'UIKit'
+    setIsNewEmployeeModalOpen(false)
+    setEmployeeList((prevEmployeeList) => prevEmployeeList.concat(newEmployeeContent))
+  }
+
+  const deleteEmployee = () => {
+    setIsDeleteModalOpen(false)
+    setEmployeeList((prevEmployeeList) => {
+      return prevEmployeeList.filter((employee) => {
+        if (employee.key !== newEmployeeContent.key) {
+          return employee
+        }
       })
-    )
+    })
+  }
+
+  const updateEmployee = () => {
+    setIsNewEmployeeModalOpen(false)
+    setEmployeeList((prevEmployeeList) => {
+      return prevEmployeeList.map((employee) => {
+        if (employee.key !== newEmployeeContent.key) {
+          return employee
+        }
+        return newEmployeeContent
+      })
+    })
   }
 
   return (
     <>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Title level={3}>Employee list</Title>
-        <Button onClick={addEmployee}>Add employee</Button>
+        <Button onClick={handleAddEmployeeButtonClick}>Add employee</Button>
       </div>
       <Search
         onChange={(e) => setSearchedEmployee(e.target.value)}
@@ -37,8 +76,25 @@ const EmployeesPage = () => {
       />
       <EmployeesList
         searchedEmployee={searchedEmployee}
+        newEmployeeContent={newEmployeeContent}
         employeeList={employeeList}
-        setEmployeeList={setEmployeeList}
+        setIsDeleteModalOpen={setIsDeleteModalOpen}
+        setIsNewEmployeeModalOpen={setIsNewEmployeeModalOpen}
+        setNewEmployeeContent={setNewEmployeeContent}
+      />
+      <NewEmployeeModal
+        newEmployeeContent={newEmployeeContent}
+        isNewEmployeeModalOpen={isNewEmployeeModalOpen}
+        setIsNewEmployeeModalOpen={setIsNewEmployeeModalOpen}
+        updateEmployee={updateEmployee}
+        addEmployee={addEmployee}
+        setNewEmployeeContent={setNewEmployeeContent}
+      />
+      <DeleteEmployeeModal
+        newEmployeeContent={newEmployeeContent}
+        isDeleteModalOpen={isDeleteModalOpen}
+        setIsDeleteModalOpen={setIsDeleteModalOpen}
+        deleteEmployee={deleteEmployee}
       />
     </>
   )
