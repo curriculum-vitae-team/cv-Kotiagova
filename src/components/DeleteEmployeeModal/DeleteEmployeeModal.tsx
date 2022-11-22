7400df746d323cca41a4a8041ac92433265ad1d8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-22 20:23:11 +0300

    Create employee delete popup

---
+++ src/components/DeleteEmployeeModal/DeleteEmployeeModal.tsx
@@ -0,0 +1,35 @@
+import { Modal } from 'antd'
+import React from 'react'
+import { Employee } from '../EmployeesList/EmployeesList'
+
+type Props = {
+  newEmployeeContent: Employee
+  isDeleteModalOpen: boolean
+  setIsDeleteModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  deleteEmployee: () => void
+}
+
+const DeleteEmployeeModal: React.FC<Props> = ({
+  newEmployeeContent,
+  isDeleteModalOpen,
+  setIsDeleteModalOpen,
+  deleteEmployee
+}) => {
+  return (
+    <>
+      <Modal
+        title='20px to Top'
+        centered
+        open={isDeleteModalOpen}
+        onOk={() => deleteEmployee()}
+        onCancel={() => setIsDeleteModalOpen(false)}
+      >
+        <p>
+          Do you want to delete {newEmployeeContent.firstName} {newEmployeeContent.lastName}?
+        </p>
+      </Modal>
+    </>
+  )
+}
+
+export default DeleteEmployeeModal
