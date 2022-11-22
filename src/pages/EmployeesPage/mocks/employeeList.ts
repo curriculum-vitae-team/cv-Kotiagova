34373a12a55c8b5dd8d7164341dc06a46ce0d375
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-22 12:09:40 +0300

    Create mocks of employees list

---
+++ src/pages/EmployeesPage/mocks/employeeList.ts
@@ -0,0 +1,36 @@
+import { Employee } from './../../../components/EmployeesList/EmployeesList'
+
+export const mockEmployeeList: Employee[] = [
+  {
+    key: '1',
+    firstName: 'Vasya',
+    lastName: 'Pupkin',
+    email: 'Vasya.Pupkin@gmail.com',
+    department: 'JavaScript',
+    specialization: 'React'
+  },
+  {
+    key: '2',
+    firstName: 'Anton',
+    lastName: 'Mihailov',
+    email: 'Anton.Mihailov@gmail.com',
+    department: 'JavaScript',
+    specialization: 'Angular'
+  },
+  {
+    key: '3',
+    firstName: 'Kirill',
+    lastName: 'Sadovnikov',
+    email: 'Kirill.Sadovnikov@gmail.com',
+    department: 'JavaScript',
+    specialization: 'Vue'
+  },
+  {
+    key: '4',
+    firstName: 'Mihail',
+    lastName: 'Novoselov',
+    email: 'Mihail.Novoselov@gmail.com',
+    department: 'JavaScript',
+    specialization: 'Svetle'
+  }
+]
