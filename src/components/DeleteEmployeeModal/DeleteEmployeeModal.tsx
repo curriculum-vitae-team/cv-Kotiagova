d66390abe3835bc883cbbc408ebf2029664cd2cb
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-23 19:36:06 +0300

    Rename props' type

--- src/components/DeleteEmployeeModal/DeleteEmployeeModal.tsx
+++ src/components/DeleteEmployeeModal/DeleteEmployeeModal.tsx
@@ -2,33 +2,31 @@ import { Modal } from 'antd'
 import React from 'react'
 import { Employee } from '../EmployeesList/EmployeesList'
 
-type Props = {
+type DeleteEmployeeModalProps = {
   newEmployeeContent: Employee
   isDeleteModalOpen: boolean
   setIsDeleteModalOpen: React.Dispatch<React.SetStateAction<boolean>>
   deleteEmployee: () => void
 }
 
-const DeleteEmployeeModal: React.FC<Props> = ({
+const DeleteEmployeeModal: React.FC<DeleteEmployeeModalProps> = ({
   newEmployeeContent,
   isDeleteModalOpen,
   setIsDeleteModalOpen,
   deleteEmployee
 }) => {
   return (
-    <>
-      <Modal
-        title={`Delete ${newEmployeeContent.firstName}`}
-        centered
-        open={isDeleteModalOpen}
-        onOk={() => deleteEmployee()}
-        onCancel={() => setIsDeleteModalOpen(false)}
-      >
-        <p>
-          Do you want to delete {newEmployeeContent.firstName} {newEmployeeContent.lastName}?
-        </p>
-      </Modal>
-    </>
+    <Modal
+      title={`Delete ${newEmployeeContent.firstName}`}
+      centered
+      open={isDeleteModalOpen}
+      onOk={() => deleteEmployee()}
+      onCancel={() => setIsDeleteModalOpen(false)}
+    >
+      <p>
+        Do you want to delete {newEmployeeContent.firstName} {newEmployeeContent.lastName}?
+      </p>
+    </Modal>
   )
 }
 
