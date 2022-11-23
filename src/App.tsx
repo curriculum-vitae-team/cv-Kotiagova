4dd9f49f23a032d16d07d5e076b5445bc4611545
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-23 19:32:14 +0300

    Set up routes, move layout code to LayoutWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,26 @@
 import '@/App.css'
 import EmployeesPage from '@/pages/EmployeesPage/EmployeesPage'
-import { Breadcrumb, Layout, Menu } from 'antd'
 import React from 'react'
-import MainSidepanel from './components/UI/sidepanels/MainSidepanel/MainSidepanel'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LayoutWrapper from './components/LayoutWrapper/LayoutWrapper'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
-  const { Header, Content, Sider } = Layout
-
   return (
     <div className='App'>
-      <Layout>
-        <Header className='header'>
-          <div className='logo' />
-          <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']} />
-        </Header>
-        <Layout>
-          <Sider style={{ height: '10%' }} width={200} className='site-layout-background'>
-            <MainSidepanel />
-          </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Employees</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content
-              className='site-layout-background'
-              style={{
-                padding: 24,
-                margin: 0,
-                minHeight: 280
-              }}
-            >
-              <EmployeesPage />
-            </Content>
-          </Layout>
-        </Layout>
-      </Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route
+            path='/employees'
+            element={
+              <LayoutWrapper>
+                <EmployeesPage />
+              </LayoutWrapper>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
