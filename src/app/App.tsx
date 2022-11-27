b51a7148924494470df4b4a87215b29bec44e3e6
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-27 10:14:28 +0300

    Dispose of css files

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
-import '@/App.css'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import AppRouter from './routes/AppRouter'
+import AppRouter from '../routes/AppRouter'
 
 function App() {
   return (
