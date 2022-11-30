235d86e18734a8c0a616db675fa90e8fd23e1db6
lnicepei <dmitrii.shaidorov@gmail.com>
2022-11-30 12:14:49 +0300

    Rewrite Redux-related code

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -2,3 +2,43 @@ declare module '*.css' {
   const css: { [key: string]: string }
   export default css
 }
+
+declare global {
+  type Employee = {
+    department: {
+      id: string
+      name: string
+      created_at: string
+    } | null
+    position: {
+      id: string
+      name: string
+      created_at: string
+    } | null
+    id: string
+    created_at: string
+    email: string
+    password: string
+    is_verified: boolean
+    profile: {
+      id: string
+      first_name: string
+      last_name: string
+    }
+    skills: [
+      {
+        skill_name: string
+        mastery: string
+      }
+    ]
+    languages: [
+      {
+        language_name: string
+        proficiency: string
+      }
+    ]
+    role: string
+  }
+}
+
+export {}
